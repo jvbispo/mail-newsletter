57b20fede16ec9b0374e24466a2386f10c182d45
jv <joaovictor.bispo@novaescola.org.br>
2021-07-22 17:09:35 -0300

    created home layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,22 @@
-import './styles.scss';
-
+import "./styles.scss";
+import HomeImage from "../../assets/home.jpg";
 
 const HomePage = () => {
+  return (
+    <div className="container">
+      <div className="center-container">
+        <section className="img-section">
+          <img src={HomeImage} alt="" />
+        </section>
+        <section className="form-section">
+          <h1>Awesome newsletter</h1>
+          <span>Digite seu email e inscreva-se</span>
+          <input type="text" />
+          <button>INSCREVER</button>
+        </section>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <h1>Teste</h1>
-    )
-}; 
-
-
-
-export {HomePage};
+export { HomePage };
